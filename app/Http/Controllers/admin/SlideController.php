42863629b0b7d7475234a81551ce75b1595d7f23
yangwenlong <2382662404@qq.com>
2020-11-07 21:02:26 +0800

    yang

--- app/Http/Controllers/admin/SlideController.php
+++ app/Http/Controllers/admin/SlideController.php
@@ -5,18 +5,167 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slide;
-
+//轮播图
 class SlideController extends Controller
 {
     public function create()
     {
-        return view('admin.slide.create');
+       if(request()->isMethod("get")){
+         $swei = Slide::orderBY("slide_weight",'desc')->first("slide_weight");
+         $name = $swei->slide_weight+1;
+         return view('admin.slide.create',["name"=>$name]);
+       }
+       if(request()->isMethod("post")){
+            $slide_url = request()->post("slide_url");
+            $Silde_name = request()->post("Silde_name");
+            $silde_weight = request()->post("silde_weight");
+            $is_show = request()->post("is_show");
+            //地址
+            $url = $_SERVER["SERVER_NAME"];
+            $img_path = request()->post("img_path");
+            $silde_log  = $url."/".$img_path;
+            $slide_time = time();
+            $namedesc = Slide::where("slide_weight",$silde_weight)->first();
+            if(!empty($namedesc)){
+                $username = [
+                    "error"=>1,
+                    "msg"=>"权重已存在，请更换",
+                ];
+                return json_encode($username);
+            }
+            $namedescs = Slide::where("slide_name",$Silde_name)->first();
+            if(!empty($namedescs)){
+                $username = [
+                    "error"=>1,
+                    "msg"=>"标题已存在，请更换",
+                ];
+                return json_encode($username);
+            }
+            $namedescsd = Slide::where("slide_url",$slide_url)->first();
+            if(!empty($namedescsd)){
+                $username = [
+                    "error"=>1,
+                    "msg"=>"地址已存在，请更换",
+                ];
+                return json_encode($username);
+            }
+            $data = [
+                "slide_url"=>$slide_url,
+                //权重
+                "slide_weight"=>$silde_weight,
+                //图片上传路径
+                "silde_log"=>$img_path,
+                //是否显示
+                "is_show"=>$is_show,
+                //添加时间
+                "slide_time"=>$slide_time,
+                //标题
+                "slide_name"=>$Silde_name,
+            ];
+            $name = Slide::insert($data);
+            if($name){
+                $username = [
+                    "error"=>0,
+                    "msg"=>"添加成功",
+                    "data"=>"/admin/slide/list",
+                ];
+                return json_encode($username);
+            }else{
+                $username = [
+                    "error"=>1,
+                    "msg"=>"添加失败",
+                ];
+                return json_encode($username);
+            }
+       }
     }
+     //上传图片
 	public function store(){
+        //接参数
+        $fileCharater = $_FILES["Filedata"];
+        //赋变量
+        $name =  $fileCharater["tmp_name"];
+        //截取后缀名
+        $ext = explode('.',$fileCharater["name"])[1];
+        //重新赋变量
+        $newfileName = MD5(time()).".".$ext;
+        //拼接
+        $newfilePath = "uploads/".$newfileName;
+        // 函数将上传的文件移动到新位置
+        move_uploaded_file($name,$newfilePath);
+        //返回参数
+        echo $newfilePath;
 	}
-
+    //展示
     public function list()
     {
-        return view('admin.slide.list');
+        $name = Slide::where("is_del",1)->get()->toArray();
+        return view('admin.slide.list',["name"=>$name]);
     }
+    //删除
+    public function del($id){
+        $fgid="/^[0-9]*$/";
+        if(!preg_match($fgid,$id)){
+            return redirect("/admin/slide/list")->with("get","请不要测试我的耐性！");
+        }
+        $name = Slide::where("slide_id",$id)->update(["is_del"=>2]);
+        if($name){
+            return redirect("/admin/slide/list")->with("get","删除成功");
+        }else{
+            return redirect("/admin/slide/list")->with("get","抱歉，请尊重PHP");
+        }
+    }
+    //修改数据
+    public function upd($id){
+        $fgid="/^[0-9]*$/";
+        if(!preg_match($fgid,$id)){
+            return redirect("/admin/slide/list")->with("get","请不要测试我的耐性！");
+        }
+        if(request()->isMethod('get')){
+            $name = Slide::where("is_del",'1')->first();
+            $swei = Slide::orderBY("slide_weight",'desc')->first("slide_weight");
+            return view("admin.slide.upd",["name"=>$name,"swei"=>$swei]);
+        }
+        if(request()->isMethod("post")){
+            $slide_url = request()->post("slide_url");
+            $Silde_name = request()->post("Silde_name");
+            $silde_weight = request()->post("silde_weight");
+            $is_show = request()->post("is_show");
+            $img_path = request()->post("img_path");
+            $slide_time = time();
+            $data = [
+                "slide_url"=>$slide_url,
+                //权重
+                "slide_weight"=>$silde_weight,
+                //图片上传路径
+                "silde_log"=>$img_path,
+                //是否显示
+                "is_show"=>$is_show,
+                //添加时间
+                "slide_time"=>$slide_time,
+                //标题
+                "slide_name"=>$Silde_name,
+            ];
+            $name = Slide::where("slide_id",$id)->update($data);
+            if($name){
+                $username = [
+                    "error"=>0,
+                    "msg"=>"修改成功",
+                    "data"=>"/admin/slide/list",
+                ];
+                return json_encode($username);
+            }else{
+                $username = [
+                    "error"=>1,
+                    "msg"=>"修改失败",
+                ];
+                return json_encode($username);
+            }
+        }
+    }
+
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::prefix('/index')->group(function () {
 /**
  * 后台
  */
-Route::prefix('admin')->group(function () {
+Route::prefix('/admin')->group(function () {
     //首页
     Route::get('/index', 'Admin\IndexController@index');
     //注册
@@ -62,9 +62,11 @@ Route::prefix('admin')->group(function () {
     Route::any('/powerList', 'Admin\PowerController@powerList');
 
     //轮播图
-    Route::any('slide/create', 'Admin\SlideController@create');
-    Route::any('slide/store', 'Admin\SlideController@store');
-    Route::any('slide/list', 'Admin\SlideController@list');
+    Route::any('/slide/create', 'admin\SlideController@create');
+    Route::any('/slide/store', 'admin\SlideController@store');
+    Route::any('/slide/list', 'admin\SlideController@list');
+    Route::any('/slide/del/{id}', 'admin\SlideController@del');
+    Route::any('/slide/upd/{id}', 'admin\SlideController@upd');
 
     //课程目录
     Route::any('/course/catalog/create', 'Admin\Course\CatalogController@create');
