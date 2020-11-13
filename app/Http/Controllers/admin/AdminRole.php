9a58a8bed06af36bccdddf369f301c99429091a7
Your Name <you@example.com>
2020-11-13 13:51:30 +0800

    123

--- app/Http/Controllers/admin/AdminRole.php
+++ app/Http/Controllers/admin/AdminRole.php
@@ -29,15 +29,4 @@ class AdminRole extends Controller
         $roleData = arr($roleData);
         return view('admin.adminRole.adminRoleAdd',['roleData'=>$roleData,'adminData'=>$adminData]);
     }
-<<<<<<< HEAD
-    public function adminRoleList()
-    {
-        $data = DB::table("shop_admin_role")->get();
-        // dump($data);
-        $data = DB::table("shop_role")->get()->toArray();
-        // dd($data);
-        return view('admin.adminRole.adminRoleList',['data'=>$data]);
-    }
-=======
->>>>>>> 3b64089c004d1db202e8255793e2713339f4f132
 }
